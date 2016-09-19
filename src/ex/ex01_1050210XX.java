88523d9eec0ae9d83d6363a5883181e869a9b456
fi1017sh <105021009@live.asia.edu.tw>
2016-09-19 16:02:37 +0800

    ex0919

--- src/ex/ex01_1050210XX.java
+++ src/ex/ex01_1050210XX.java
@@ -2,10 +2,10 @@ package ex;
 /*
  * Topic: 請寫一個程式讓使用者輸入「華氏」溫度，輸出「攝氏」溫度 (華氏 = 攝氏*(9/5)+32)
  * Date: 2016/09/19
- * Author: 1050210XX 周永振老師
+ * Author: 105021009  陳俞儒
  */
 
-public class ex01_1050210XX {
+public class ex01_105021009{
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
--- src/ex/ex02_1050210XX.java
+++ src/ex/ex02_1050210XX.java
@@ -2,14 +2,20 @@ package ex;
 /*
  * Topic: 請寫一個程式讓使用者輸入體重單位 "公斤"，轉換成 "磅" (1磅 = 0.45359 公斤)
  * Date: 2016/09/19
- * Author: 1050210XX 周永振老師
+ * Author: 105021009  陳俞儒
  */
 
-public class ex02_1050210XX {
-
+public class ex02_105021009 {
+	
+	import java.until.Scanner;
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+	
+	Scanner scn = new Scanner(System in);
+	x=scanner.next();
+	System.out.print("公斤數");
+	System.out.print("磅" + (x/0.45359));
 	}
 
 }
